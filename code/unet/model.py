0d38cf654a619fffef1bd2144fb72ece1dcde672
Arcturion <dimascours@gmail.com>
2024-03-02 18:52:41 +0800

    rewrite model.py
    rewrite

--- code/unet/model.py
+++ code/unet/model.py
@@ -9,10 +9,9 @@ def dice_coef(y_true, y_pred):
     intersection = keras.backend.sum(y_true_f * y_pred_f)
     return (2. * intersection) / (keras.backend.sum(y_true_f + y_pred_f))
 
+
 class inpaintingModel:
-  '''
-  Build UNET like model for image inpaining task.
-  '''
+
   def prepare_model(self, input_size=(256,256,3)):
     inputs = keras.layers.Input(input_size)
 
@@ -34,46 +33,23 @@ class inpaintingModel:
     return keras.models.Model(inputs=[inputs], outputs=[outputs])
 
   def __ConvBlock(self, filters, kernel_size, pool_size, activation, padding, connecting_layer, pool_layer=True):
-      
-      conv = keras.layers.Conv2D(filters=filters, kernel_size=kernel_size, activation=activation, padding=padding, kernel_regularizer=tf.keras.regularizers.l2(l2_reg), bias_regularizer=tf.keras.regularizers.l1(l1_reg))(connecting_layer)
-      conv = keras.layers.Conv2D(filters=filters, kernel_size=kernel_size, activation=activation, padding=padding, kernel_regularizer=tf.keras.regularizers.l2(l2_reg), bias_regularizer=tf.keras.regularizers.l1(l1_reg))(conv)
-      
-      if pool_layer:
-          pool = keras.layers.MaxPooling2D(pool_size)(conv)
+    conv = keras.layers.Conv2D(filters=filters, kernel_size=kernel_size, activation=activation, padding=padding)(connecting_layer)
+    conv = keras.layers.Conv2D(filters=filters, kernel_size=kernel_size, activation=activation, padding=padding)(conv)
+    if pool_layer:
+      pool = keras.layers.MaxPooling2D(pool_size)(conv)
       return conv, pool
-      else:
+    else:
       return conv
 
-   def __UpConvBlock(self, filters, up_filters, kernel_size, up_kernel, up_stride, activation, padding, connecting_layer, shared_layer, l1_reg=0.001, l2_reg=0.001):
-    
-      conv = keras.layers.Conv2D(filters=filters, kernel_size=kernel_size, activation=activation, padding=padding, kernel_regularizer=tf.keras.regularizers.l2(l2_reg), bias_regularizer=tf.keras.regularizers.l1(l1_reg))(connecting_layer)
-      conv = keras.layers.Conv2D(filters=filters, kernel_size=kernel_size, activation=activation, padding=padding, kernel_regularizer=tf.keras.regularizers.l2(l2_reg), bias_regularizer=tf.keras.regularizers.l1(l1_reg))(conv)
-    
-      up = keras.layers.Conv2DTranspose(filters=up_filters, kernel_size=up_kernel, strides=up_stride, padding=padding)(conv)
-      up = keras.layers.concatenate([up, shared_layer], axis=3)
+  def __UpConvBlock(self, filters, up_filters, kernel_size, up_kernel, up_stride, activation, padding, connecting_layer, shared_layer):
+    conv = keras.layers.Conv2D(filters=filters, kernel_size=kernel_size, activation=activation, padding=padding)(connecting_layer)
+    conv = keras.layers.Conv2D(filters=filters, kernel_size=kernel_size, activation=activation, padding=padding)(conv)
+    up = keras.layers.Conv2DTranspose(filters=up_filters, kernel_size=up_kernel, strides=up_stride, padding=padding)(conv)
+    up = keras.layers.concatenate([up, shared_layer], axis=3)
 
     return conv, up
 
-    def select_channel(self, x, n):
-        x = x[:, :, :, n]
-        x = Reshape((x.shape[1], x.shape[2], 1))(x)
+  def select_channel(self, x, n):
+    x = x[:, :, :, n]
+    x = Reshape((x.shape[1], x.shape[2], 1))(x)
     return x
-
-
-
-
-keras.backend.clear_session()
-# load the pre-trained model
-wrapper_model = inpaintingModel().prepare_model()
-wrapper_model.load_weights('/content/drive/MyDrive/transfer_learning_SST/weights.30-0.0097.hdf5')
-
-image_input = Input(shape=(256, 256, 3))
-
-outputs1 = wrapper_model(image_input)
-
-image_input2 = tf.slice(image_input, [0, 0, 0, 1], [-1, 256, 256, 2]) # shape (1, 256, 256, 2), remove the first channel
-arr = tf.concat([outputs1, image_input2], -1) # shape (1, 256, 256, 3), replace the first channel
-
-outputs2 = wrapper_model(arr)
-
-#new_model = Model(inputs=[image_input], outputs=[outputs2])
