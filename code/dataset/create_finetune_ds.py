cc19898e0222e14983155b44b4238d3016eeb1d5
Arcturion <dimascours@gmail.com>
2024-03-03 20:16:06 +0800

    Update create_finetune_ds.py
    reconstruct the code

--- code/dataset/create_finetune_ds.py
+++ code/dataset/create_finetune_ds.py
@@ -3,54 +3,60 @@ from numpy import load
 import numpy as np
 import random
 
-from sklearn.preprocessing import MinMaxScaler
 from sklearn.utils import shuffle
 
-def make_sequence(data_raw, look_back):
-    data = []
 
-    # create all possible sequences of length seq_len
-    for index in range(len(data_raw) - look_back):
-        data.append(data_raw[index: index + look_back])
-
-    data = np.array(data);
-
-    x_train = np.array(data[:,:,:,:])
-    y_train = np.array(data[:,-1,:,:])
+def make_sequences(data_raw, look_back):
+    sequences = [data_raw[index: index + look_back] for index in range(len(data_raw) - look_back)]
+    sequences = np.array(sequences)
+    x_train = sequences
+    y_train = sequences[:, -1]
 
     return x_train, y_train
+
+# MinMax Scaler
+def min_max_scaler(data):
+    masked_data = data[data > 0]
+    #min_val = np.min(masked_data)
+    #max_val = np.max(data)
+    min_val = 5
+    max_val = 35
+    data[data > 0] = (masked_data - min_val) / (max_val - min_val)
+    return data
 
 
 sat_data_1 = load('2015sampai2020.npy')
 sat_data_2 = load('2020sampai2022.npy')
 
 #scalling
-scaler_1 = MinMaxScaler().fit(np.reshape(sat_data_1, (sat_data_1.shape[0],sat_data_1.shape[1]*sat_data_1.shape[2])))
-data_scaled_1 = scaler_1.transform(np.reshape(sat_data_1, (sat_data_1.shape[0],sat_data_1.shape[1]*sat_data_1.shape[2])))
+data_scaled_1 = min_max_scaler(np.reshape(sat_data_1, (sat_data_1.shape[0],sat_data_1.shape[1]*sat_data_1.shape[2])))
 sat_data_1 = np.reshape(data_scaled_1, (sat_data_1.shape[0],sat_data_1.shape[1],sat_data_1.shape[2]))
 
-scaler_2 = MinMaxScaler().fit(np.reshape(sat_data_2, (sat_data_2.shape[0],sat_data_2.shape[1]*sat_data_2.shape[2])))
-data_scaled_2 = scaler_2.transform(np.reshape(sat_data_2, (sat_data_2.shape[0],sat_data_2.shape[1]*sat_data_2.shape[2])))
+data_scaled_2 = min_max_scaler(np.reshape(sat_data_2, (sat_data_2.shape[0],sat_data_2.shape[1]*sat_data_2.shape[2])))
 sat_data_2 = np.reshape(data_scaled_2, (sat_data_2.shape[0],sat_data_2.shape[1],sat_data_2.shape[2]))
 
 # Make Sequence
-sat_data_1, sat_data_1_y = make_sequence(sat_data_1, 3)
+sat_data_1, sat_data_1_y = make_sequences(sat_data_1, 3)
 sat_data_1 = np.transpose(sat_data_1, (0, 2, 3, 1))
 
-sat_data_2, sat_data_2_y = make_sequence(sat_data_2, 3)
+sat_data_2, sat_data_2_y = make_sequences(sat_data_2, 3)
 sat_data_2 = np.transpose(sat_data_2, (0, 2, 3, 1))
 
-#concat both dataset
+
+random_number = 20   ### Change the state each iteration
+
+#Join
 sat_data = np.concatenate((sat_data_1, sat_data_2), axis=0)
 sat_data_y = np.concatenate((sat_data_1_y, sat_data_2_y), axis=0)
 
-#multiply numbers
+sat_data = np.flip(sat_data, axis = 1)
+sat_data_y = np.flip(sat_data_y, axis = 1)
+
+#Multipy
 #sat_data = np.repeat(sat_data, 3, axis=0)
 #sat_data_y = np.repeat(sat_data_y, 3, axis=0)
 
-sat_data, sat_data_y = shuffle(sat_data, sat_data_y, random_state=16)
-
-
+sat_data, sat_data_y = shuffle(sat_data, sat_data_y, random_state=random_number)
 
 real_value = np.expand_dims(sat_data[:,:,:,0], axis= -1)
 
